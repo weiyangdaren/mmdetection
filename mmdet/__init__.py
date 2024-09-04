604b4dbfdb7dcbf138b7cea756a377abb79e48e1
weiyangdaren <ccail2022@outlook.com>
2024-09-04 09:28:28 +0800

    support mmcv==2.2.0

--- mmdet/__init__.py
+++ mmdet/__init__.py
@@ -14,9 +14,9 @@ mmengine_maximum_version = '1.0.0'
 mmengine_version = digit_version(mmengine.__version__)
 
 assert (mmcv_version >= digit_version(mmcv_minimum_version)
-        and mmcv_version < digit_version(mmcv_maximum_version)), \
+        and mmcv_version <= digit_version(mmcv_maximum_version)), \
     f'MMCV=={mmcv.__version__} is used but incompatible. ' \
-    f'Please install mmcv>={mmcv_minimum_version}, <{mmcv_maximum_version}.'
+    f'Please install mmcv>={mmcv_minimum_version}, <={mmcv_maximum_version}.'
 
 assert (mmengine_version >= digit_version(mmengine_minimum_version)
         and mmengine_version < digit_version(mmengine_maximum_version)), \
